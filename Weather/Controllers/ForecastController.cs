ae432adf6fbf8f4836bf2c98a5034f6568ae12de
inark <inark@DESKTOP-I2TATUK>
2020-06-11 15:29:44 +0600

    Forecast application

---
+++ Weather/Controllers/ForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace Weather.Controllers
+{
+    public class ForecastController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View(GetForecastWeather());
+        }
+
+        public Models.OneCallApi GetForecastWeather()
+        {
+            var jsonString = string.Empty;
+            string url = $"{ConfigurationManager.AppSettings["OpenweatherApiUri"]}?lat={ConfigurationManager.AppSettings["lat"]}" +
+                $"&lon={ConfigurationManager.AppSettings["lon"]}&exclude=current,minutely,hourly&appid={ConfigurationManager.AppSettings["ApiKey"]}" +
+                $"&units={ConfigurationManager.AppSettings["Units"]}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                jsonString = reader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<Models.OneCallApi>(jsonString);
+        }
+    }
+}
---
+++ Weather/Models/Weather.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Weather.Models
+{
+
+    public class Temp
+    {
+
+        [JsonProperty("day")]
+        public double day { get; set; }
+
+        [JsonProperty("min")]
+        public double min { get; set; }
+
+        [JsonProperty("max")]
+        public double max { get; set; }
+
+        [JsonProperty("night")]
+        public double night { get; set; }
+
+        [JsonProperty("eve")]
+        public double eve { get; set; }
+
+        [JsonProperty("morn")]
+        public double morn { get; set; }
+    }
+
+    public class FeelsLike
+    {
+
+        [JsonProperty("day")]
+        public double day { get; set; }
+
+        [JsonProperty("night")]
+        public double night { get; set; }
+
+        [JsonProperty("eve")]
+        public double eve { get; set; }
+
+        [JsonProperty("morn")]
+        public double morn { get; set; }
+    }
+
+    public class Weather
+    {
+
+        [JsonProperty("id")]
+        public int id { get; set; }
+
+        [JsonProperty("main")]
+        public string main { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+
+        [JsonProperty("icon")]
+        public string icon { get; set; }
+    }
+
+    public class Daily
+    {
+
+        [JsonProperty("dt")]
+        public int dt { get; set; }
+
+        [JsonProperty("sunrise")]
+        public int sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public int sunset { get; set; }
+
+        [JsonProperty("temp")]
+        public Temp temp { get; set; }
+
+        [JsonProperty("feels_like")]
+        public FeelsLike feels_like { get; set; }
+
+        [JsonProperty("pressure")]
+        public int pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public int humidity { get; set; }
+
+        [JsonProperty("dew_point")]
+        public double dew_point { get; set; }
+
+        [JsonProperty("wind_speed")]
+        public double wind_speed { get; set; }
+
+        [JsonProperty("wind_deg")]
+        public int wind_deg { get; set; }
+
+        [JsonProperty("weather")]
+        public IList<Weather> weather { get; set; }
+
+        [JsonProperty("clouds")]
+        public int clouds { get; set; }
+
+        [JsonProperty("rain")]
+        public double rain { get; set; }
+
+        [JsonProperty("uvi")]
+        public double uvi { get; set; }
+    }
+
+    public class OneCallApi
+    {
+
+        [JsonProperty("lat")]
+        public double lat { get; set; }
+
+        [JsonProperty("lon")]
+        public double lon { get; set; }
+
+        [JsonProperty("timezone")]
+        public string timezone { get; set; }
+
+        [JsonProperty("timezone_offset")]
+        public int timezone_offset { get; set; }
+
+        [JsonProperty("daily")]
+        public IList<Daily> daily { get; set; }
+    }
+
+}
